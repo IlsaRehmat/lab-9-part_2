f69479b2065130dcc62e3f18eb5fb3429e0ff47f
Ilsa <irehmat123@gmail.com>
2025-04-06 14:25:40 -0400

    Question 28 part 3

---
+++ Player.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private String name;
+    private Room currentRoom;
+    private List<Item> inventory;
+    
+    public Player(String name, Room startingRoom){
+        this.name = name;
+        this.currentRoom = startingRoom;
+        this.inventory = new ArrayList<>();
+    }
+    
+    public Room getCurrentRoom(){
+        return currentRoom;
+    }
+    
+    public void setCurrentRoom( Room room){
+        this.currentRoom = room;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public List<Item> getInventory(){
+        return inventory;
+    }
+    
+    public void addItem(Item item){
+        inventory.add(item);
+    }
+    
+    public void removeitem(Item item){
+        inventory.remove(item);
+    }
+}
+
